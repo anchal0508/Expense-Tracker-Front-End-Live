bba7d78d10311b18af81828b31120844b3979817
anchal.koshta007@gmail.com <genious@genious.genious>
2026-07-12 20:03:41 +0530

    Completed with Password reset2

--- src/Site/Components/SignUp.tsx
+++ src/Site/Components/SignUp.tsx
@@ -1,7 +1,6 @@
 import { Bubbles, Loader } from "lucide-react";
 import type React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import API from "../../axiosConfig";
 interface signupDetails {
     name: string,
